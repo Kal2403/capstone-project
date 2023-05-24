3972c1a7726a3203deb8ec41e0d943b6e2f5ccfb
Cristhian Castillo <97223412+Kal2403@users.noreply.github.com>
2023-05-24 03:01:04 +0200

    Added display menu funtionally and fix linters

--- script.js
+++ script.js
@@ -1,47 +1,54 @@
-// FEATURED SPEAKERS
+const openMenu = () => {
+  const menu = document.getElementById('nav');
+  menu.style.display = 'block';
+};
+
+function closeMenu() {
+  const menu = document.getElementById('nav');
+  menu.style.display = 'none';
+}
+
+openMenu();
+closeMenu();
+
 const dataSpeakers = [
   {
-  name: 'Tim Berners-Lee',
-  img: 'img/TimBernersLee.jpg',
-  profession: 'He is a British computer scientist and physicist',
-  achievements: 'credited with the title of inventor of the World Wide Web (WWW).'
-},
-
-{
-  name: 'Brendan Eich',
-  img: 'img/brendaneich.jpg',
-  profession: 'He is an American programmer and software developer',
-  achievements: 'He co-founded Mozilla and worked as the CTO (Chief Technology Officer) at Brave Software.'
-},
-
-{
-  name: 'Linus Torvalds',
-  img: 'img/LinusTorvalds.jpeg',
-  profession: 'He is a Finnish software engineer.',
-  achievements: 'Creator of the Linux operating system. While not directly related to web development, Linux is widely used in web servers.'
-},
-
-{
-  name: 'Marc Andreessen',
-  img: 'img/MarcAndreessen.jpg',
-  profession: 'He is an American entrepreneur and venture capitalist',
-  achievements: 'He co-founded Netscape Communications Corporation and held executive positions there.'
-},
-
-{
-  name: 'Jeffrey Zeldman',
-  img: 'img/JeffreyZeldman.jpg',
-  profession: 'He is an American web designer and writer',
-  achievements: 'He is the co-founder of A List Apart, an influential online publication on web design and development.'
-},
-
-{
-  name: 'Matt Mullenweg',
-  img: 'img/MattMullenweg.jpeg',
-  profession: 'He is an American entrepreneur and programmer.',
-  achievements: 'He is the co-founder and lead developer of WordPress, and has held executive positions at Automattic, the parent company of WordPress.com.'
-}
-]
+    name: 'Tim Berners-Lee',
+    img: 'img/TimBernersLee.jpg',
+    profession: 'He is a British computer scientist and physicist',
+    achievements: 'credited with the title of inventor of the World Wide Web (WWW).',
+  },
+  {
+    name: 'Brendan Eich',
+    img: 'img/brendaneich.jpg',
+    profession: 'He is an American programmer and software developer',
+    achievements: 'He co-founded Mozilla and worked as the CTO (Chief Technology Officer) at Brave Software.',
+  },
+  {
+    name: 'Linus Torvalds',
+    img: 'img/LinusTorvalds.jpeg',
+    profession: 'He is a Finnish software engineer.',
+    achievements: 'Creator of the Linux operating system. While not directly related to web development, Linux is widely used in web servers.',
+  },
+  {
+    name: 'Marc Andreessen',
+    img: 'img/MarcAndreessen.jpg',
+    profession: 'He is an American entrepreneur and venture capitalist',
+    achievements: 'He co-founded Netscape Communications Corporation and held executive positions there.',
+  },
+  {
+    name: 'Jeffrey Zeldman',
+    img: 'img/JeffreyZeldman.jpg',
+    profession: 'He is an American web designer and writer',
+    achievements: 'He is the co-founder of A List Apart, an influential online publication on web design and development.',
+  },
+  {
+    name: 'Matt Mullenweg',
+    img: 'img/MattMullenweg.jpeg',
+    profession: 'He is an American entrepreneur and programmer.',
+    achievements: 'He is the co-founder and lead developer of WordPress, and has held executive positions at Automattic, the parent company of WordPress.com.',
+  },
+];
 
 const sectionSpeakers = document.querySelector('#speakers');
 const h2 = document.createElement('h2');
@@ -56,8 +63,8 @@ const speakContainer = document.createElement('div');
 speakContainer.classList.add('speakContainer');
 sectionSpeakers.appendChild(speakContainer);
 
-for(let i = 0; i < dataSpeakers.length; i += 1) {
-  let dataSpeak = dataSpeakers[i];
+for (let i = 0; i < dataSpeakers.length; i += 1) {
+  const dataSpeak = dataSpeakers[i];
 
   const speakCard = document.createElement('div');
   speakCard.classList.add('speakCard');
@@ -88,7 +95,7 @@ for(let i = 0; i < dataSpeakers.length; i += 1) {
   p2.textContent = dataSpeak.achievements;
   descriptionCard.appendChild(p2);
 
-  if(i > 1 && i < 6) {
+  if (i > 1 && i < 6) {
     speakCard.classList.add('hideCard');
   }
 }
